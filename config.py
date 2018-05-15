7e52ad429bd2d905c608ff54906b9fc96be19ed4
Forrest Beck <forrest_beck@icloud.com>
2018-05-15 15:40:52 -0400

    end of day

--- config.py
+++ config.py
@@ -1,33 +1,39 @@
 import sys
 import os
-import pickle
+import shelve
 
 if sys.platform == "darwin":
     # OS X
-    config_file = os.environ['HOME'] + '/Library/Preferences/vc-adp-sync.pickle'
-    if not os.path.isfile(config_file):
+    config_file = os.environ['HOME'] + '/Library/Preferences/vc-adp-sync'
+    if not os.path.isfile(config_file + ".db"):
         default = {
             "vcuser": str(" "),
             "vcpass": str(" "),
             "vcurl": str(" ")
         }
-        pickle.dump(default, open(config_file, "wb"))
+        d = shelve.open(config_file, flag='c', writeback=True)
+        d["config"] = default
+        d.sync()
+        d.close()
 
 elif sys.platform == "win32":
     # Windows
     config_path = os.environ['LOCALAPPDATA'] + "/vc-adp-sync"
-    config_file = 'vc-adp-sync.pickle'
+    config_file = config_path + "/vc-adp-sync"
 
     if not os.path.isdir(config_path):
         os.makedirs(config_path)
 
-    if not os.path.isfile(config_path + "/" + config_file):
+    if not os.path.isfile(config_file + ".db"):
         default = {
             "vcuser": str(" "),
             "vcpass": str(" "),
             "vcurl": str(" ")
         }
-        pickle.dump(default, open(config_file, "wb"))
+        d = shelve.open(config_file, flag='c', writeback=True)
+        d["config"] = default
+        d.sync()
+        d.close()
 
 
 def save_settings(settings):
@@ -36,7 +42,10 @@ def save_settings(settings):
     :param settings: dictionary of settings. vcuser, vcpass, vcurl
     :return: nothing
     """
-    pickle.dump(settings, open(config_file, "wb"))
+    d = shelve.open(config_file, flag='c', writeback=True)
+    d["config"] = settings
+    d.sync()
+    d.close()
 
 
 def load_settings():
@@ -44,5 +53,36 @@ def load_settings():
     Get settings from pickle file
     :return: dictionary of settigns
     """
-    s = pickle.load(open(config_file, "rb"))
+    d = shelve.open(config_file, flag='c', writeback=True)
+    s = d["config"]
+    d.close()
     return s
+
+
+def save_field_maps(maps):
+    """
+    Saves field maps to config file
+    :param maps:
+    :return: nothing
+    """
+    try:
+        d = shelve.open(config_file, flag='c', writeback=True)
+        d["fields"] = maps
+        d.sync()
+        d.close()
+    except:
+        return None
+
+
+def load_field_maps():
+    """
+    Returns field maps from config file
+    :return:
+    """
+    try:
+        d = shelve.open(config_file, flag='c', writeback=True)
+        s = d["fields"]
+        d.close()
+        return s
+    except:
+        return None
--- main.py
+++ main.py
@@ -13,9 +13,6 @@ class Main(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        # Set Vars
-        self.vcfsdata = []
-
         # Set Images
         self.ui.logo_label.setPixmap(QtGui.QPixmap(":/images/adp-vc-logo-100.png"))
         self.ui.vcDataFSStatusLabel.setPixmap(QtGui.QPixmap(":/images/red_status.png"))
@@ -23,19 +20,19 @@ class Main(QtWidgets.QMainWindow):
 
         # Gather Config
         self.c = config.load_settings()
-
-        # Create VC Instance
-        self.vc = v.Veracross()
+        self.field_maps = config.load_field_maps()
 
         # Set Labels
         self.ui.vc_api_user.setText(self.c["vcuser"])
         self.ui.vc_api_pass.setText(self.c["vcpass"])
         self.ui.vc_api_url.setText(self.c["vcurl"])
+        self.ui.txt_fieldMap.setText(self.field_maps)
 
         # Connect buttons to methods
         self.ui.getVCDataButton.clicked.connect(self.get_vc_data)
         self.ui.settingsSave.clicked.connect(self.save_settings_button)
         self.ui.parseVCDataButton.clicked.connect(self.parse_vc_data)
+        self.ui.btn_saveFieldMap.clicked.connect(self.save_field_map)
 
     def get_vc_data(self):
         """
@@ -43,7 +40,10 @@ class Main(QtWidgets.QMainWindow):
         :return:
         """
         try:
+            self.vcfsdata = []
+            self.vc = v.Veracross()
             self.vcfsdata = self.vc.pull(self.c, "facstaff")
+
             if len(self.vcfsdata) > 0:
                 self.ui.vcFSRecordCount.setText(str(len(self.vcfsdata)) + " Faculty Staff Records")
                 self.ui.vcDataFSStatusLabel.setPixmap(QtGui.QPixmap(":/images/green_status.png"))
@@ -62,7 +62,8 @@ class Main(QtWidgets.QMainWindow):
         for i in self.vcfsdata:
 
             if i["household_fk"] > 0:
-                hh = self.vc.pull(self.c, "households/" + str(i["household_fk"]))
+                h = v.Veracross()
+                hh = h.pull(self.c, "households/" + str(i["household_fk"]))
             else:
                 hh = None
 
@@ -86,6 +87,7 @@ class Main(QtWidgets.QMainWindow):
                 a.update({"postal_code": str(hh["household"]["postal_code"])})
 
             d.insert(int(i["person_pk"]), a)
+            del(hh)
 
         if len(d) > 0:
             self.ui.lineEditXRateLimitReading.setText(self.vc.rate_limit_remaining)
@@ -109,6 +111,16 @@ class Main(QtWidgets.QMainWindow):
         # Reload Settings
         self.c = config.load_settings()
 
+    def save_field_map(self):
+        """
+        Saves the field maps to the config file
+        :return:
+        """
+        # Save settings
+        config.save_field_maps(self.ui.txt_fieldMap.toPlainText())
+        # Reload Settings
+        self.field_maps = config.load_field_maps()
+
     def debug_append_log(self, text):
         """
         Write to log window
@@ -117,7 +129,7 @@ class Main(QtWidgets.QMainWindow):
         """
         self.ui.textLog.moveCursor(QtGui.QTextCursor.End)
         self.ui.textLog.ensureCursorVisible()
-        self.ui.textLog.insertPlainText(text)
+        self.ui.textLog.insertHtml(text + "<br />")
 
 
 if __name__ == '__main__':
