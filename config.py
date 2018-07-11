cbd0220af241ede16a0e549871601b84b3b9659e
Forrest Beck <forrest_beck@icloud.com>
2018-07-11 18:08:36 -0400

    settings

--- config.py
+++ config.py
@@ -2,25 +2,38 @@ import sys
 import os
 import shelve
 
+default_config = dict()
+default_fields = "{'first_name': 'person/legalName/givenName', \n" \
+                  "'last_name': 'person/legalName/familyName1', \n}"
+
 if sys.platform == "darwin":
     # OS X
     config_file = os.environ['HOME'] + '/Library/Preferences/vc-adp-sync'
+    if not os.path.isfile(config_file + ".db"):
+        d = shelve.open(config_file, flag='c', writeback=True)
+        d["config"] = default_config
+        d["fields"] = default_fields
+        d.sync()
+        d.close()
 
 elif sys.platform == "win32":
     # Windows
     config_file = os.environ['LOCALAPPDATA'] + "/vc-adp-sync"
+    if not os.path.isfile(config_file + ".dat"):
+        d = shelve.open(config_file, flag='c', writeback=True)
+        d["config"] = default_config
+        d["fields"] = default_fields
+        d.sync()
+        d.close()
 
 else:
     config_file = "vc-adp-sync"
-
-if not os.path.isfile(config_file + ".db"):
-    default = dict()
-    d = shelve.open(config_file, flag='c', writeback=True)
-    d["config"] = default
-    d["fields"] = "{'first_name': 'person/legalName/givenName', \n" \
-                  "'last_name': 'person/legalName/familyName1', \n}"
-    d.sync()
-    d.close()
+    if not os.path.isfile(config_file):
+        d = shelve.open(config_file, flag='c', writeback=True)
+        d["config"] = default_config
+        d["fields"] = default_fields
+        d.sync()
+        d.close()
 
 
 def save_settings(settings, key):
